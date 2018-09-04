26734fd89bb4830d1ac2c35c575ec70940401318
Chris Vincent <c.j.vincent@gmail.com>
2018-09-04 15:38:14 -0500

    More robust message sending...
    For some reason, the original implementation *sometimes* stopped working
    for 1-to-1 messages, including conversations it had previously worked
    with. This more robust approach seems to work consistently.

--- app/services/sender.rb
+++ app/services/sender.rb
@@ -1,6 +1,11 @@
 class Sender
   def self.send(chat_id, message, attachment_path = nil)
-    if attachment_path.nil?
+    _, service, _group, phone =
+      */\A(SMS|iMessage);(-|\+);\+?((chat)?\d+)\Z/.match(chat_id)
+
+    if /\Achat\d+\Z/.match?(phone)
+      # Group chats
+      puts "Sending to group #{phone}..."
       osascript <<~OSA
         tell application "Messages"
           set chat_id to #{chat_id.inspect}
@@ -9,6 +14,32 @@ class Sender
           send message to conversation
         end tell
       OSA
+
+    elsif service == "iMessage"
+      # For individual iMessage conversations
+      puts "Sending iMessage to #{phone}..."
+      osascript <<~OSA
+        tell application "Messages"
+          set message to #{message.inspect}
+          set msg_service to 1st service whose service type = iMessage
+          set my_buddy to buddy #{phone.inspect} of msg_service
+          send message to my_buddy
+        end tell
+      OSA
+
+    elsif service == "SMS"
+      # Slightly different approach needed for SMS
+      puts "Sending SMS to #{phone}..."
+      osascript <<~OSA
+        tell application "Messages"
+          set message to #{message.inspect}
+          set my_buddy to buddy #{phone.inspect} of service "SMS"
+          send message to my_buddy
+        end tell
+      OSA
+
+    else
+      raise "Unrecognized service #{service.inspect}!"
     end
   end
 
